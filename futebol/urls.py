a74200d1a2e0f02bcd2dc73ff9a0508724a44b84
ThiagoSilvakm35 <thiagoace99freelas@gmail.com>
2025-01-28 18:22:22 -0300

    ajuste

--- futebol/urls.py
+++ futebol/urls.py
@@ -4,11 +4,6 @@ from django.urls import path
 from jogos.api import  campeonato_router
 from jogos.api import  jogo_router
 from jogos.api import  emissora_router
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 api = NinjaAPI(title="Sistema de Extração de Jogos")
 
@@ -22,8 +17,5 @@ api.add_router("/emissoras/", emissora_router)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
 
+]
